56a4c731f347619e8c7001552eca59df4ded8e6e
Nick <xadmin@bk.ru>
2022-12-10 13:55:23 +0400

    Домашнее задание 2

---
+++ dz2_2.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class dz2_2 {
+    public static void main(String[] args) {
+        int[] mas = { 111, 32, 1, 126, 7, -1, 1 };
+        boolean isSorted = false;
+        int buf;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] > mas[i + 1]) {
+                    isSorted = false;
+                    buf = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+    }
+}
--- dz_2.java
+++ dz_2.java
@@ -1,33 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class dz_2 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Вывести все простые числа от 1 до 1000: ");
         int input = 1000;
-        scanner.close();
+        // создаем список
         List<Integer> primes = new ArrayList<>();
-
-        // loop through the numbers one by one (edit include input in range)
+        // перебираем цифры по порядку
         for (int i = 2; i <= input; i++) {
+            // ставим флаг
             boolean isPrimeNumber = true;
-
-            // check to see if the number is prime
+            // вторая итерация
             for (int j = 2; j < i; j++) {
+                // проверяем чило делится оно на себя без остатка
                 if (i % j == 0) {
+                    // меняем флаг
                     isPrimeNumber = false;
-                    break; // exit the inner for loop
+                    // как нам на лекции говорили что не красиво брейками заканчивать итерацию. но
+                    // нет выхода
+                    break;
                 }
             }
-
-            // print the number if prime
+            // проверяем флаг
             if (isPrimeNumber) {
+                // флаг Истина, значит записываем в переменную
                 primes.add(i);
             }
         }
+        // удаляем скобки и выводим результат
         String answer = primes.toString().replace("[", "").replace("]", "");
         System.out.println("Всего чисел: " + primes.size());
         System.out.println(answer);
