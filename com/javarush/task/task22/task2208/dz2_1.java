c97ec1b3fc52700b44cceb84d101c7599e6715f2
Nick <xadmin@bk.ru>
2022-12-10 14:31:12 +0400

    Create dz2_1.java

---
+++ com/javarush/task/task22/task2208/dz2_1.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task22.task2208;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class dz2_1 {
+    public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", "Obama");
+        map.put("country", "USA");
+        map.put("city", "NY");
+        map.put("age", null);
+        System.out.println("\nSELECT * FROM students WHERE " + getQuery(map) + "\n");
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder WHERE = new StringBuilder();
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            if (pair.getValue() != null) {
+                WHERE.append(pair.getKey() + " = " + "'" + pair.getValue() + "'" + " and ");
+            }
+        }
+        WHERE.delete(WHERE.length() - 4, WHERE.length());
+        return WHERE.toString();
+    }
+}
